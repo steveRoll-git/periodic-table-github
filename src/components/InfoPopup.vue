d8d576e019ade6380139e3f7f8061bc6d04c32e1
Ron Tseytlin <ronts390@gmail.com>
2023-09-15 22:21:35 +0300

    added element's summary to InfoPopup
    shows after the mouse stays for some time on the element

--- src/components/InfoPopup.vue
+++ src/components/InfoPopup.vue
@@ -3,7 +3,7 @@ import type { ElementDef } from '@/elements'
 import { onMounted, ref } from 'vue'
 
 /** How long to wait after the popup is shown before expanding it. */
-const expansionDelay = 0.5
+const expansionDelay = 750
 
 const isExpanded = ref(false)
 
@@ -22,6 +22,9 @@ onMounted(() => {
   <Transition appear name="slide-fade">
     <div class="infoPopup">
       <div class="elementTitle">{{ elementDef.name }}</div>
+      <div class="summaryWrapper" :class="{ open: isExpanded }">
+        <div class="summaryInner">{{ elementDef.summary }}</div>
+      </div>
     </div>
   </Transition>
 </template>
@@ -32,9 +35,10 @@ onMounted(() => {
   top: 50%;
   translate: 0 -50%;
   inset-inline-start: calc(100% + 5px);
+  width: v-bind('isExpanded ? "250px" : "auto"');
   z-index: 1;
 
-  padding: 5px;
+  padding: 6px;
   background-color: white;
   border-radius: 8px;
   box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.3);
@@ -42,5 +46,22 @@ onMounted(() => {
 
 .elementTitle {
   font-size: 24px;
+}
+
+.summaryWrapper {
+  display: grid;
+  grid-template-rows: 0fr;
+  /* grid-template-columns: 0fr; */
+  transition: grid-template-rows 0.5s ease-out /*, grid-template-columns 0.5s ease-out*/;
+}
+
+.summaryWrapper.open {
+  grid-template-rows: 1fr;
+  /* grid-template-columns: 1fr; */
+}
+
+.summaryInner {
+  overflow: hidden;
+  padding-top: 6px;
 }
 </style>
