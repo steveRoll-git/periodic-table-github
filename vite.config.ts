557a7831272f09b4e7aae2319d877ccdb114f4e9
Ron Tseytlin <ronts390@gmail.com>
2023-09-16 02:44:42 +0300

    changed base url

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: "https://steveroll-git.github.io/periodic-table-github/"
 })
